a932ca7f3deb3ccdc468b969c97f024e70fa368a
unknown <tsetenl2@illinois.edu>
2019-12-16 14:41:48 -0500

    Use LDA Mallet model. Remove non ascii characters and words with length less than 3 from documents

--- analyze_documents.py
+++ analyze_documents.py
@@ -1,48 +1,54 @@
-from os import listdir
+from os import listdir, environ
 from os.path import isfile, join, dirname, realpath
 from gensim import corpora
-from gensim.models import LdaModel, Phrases
+from gensim.models import Phrases
+from gensim.models.wrappers import LdaMallet
 
-NUM_TOPICS = 10
-NUM_PASSES = 20
+NUM_TOPICS = 5
 ITERATIONS = 300
-CHUNKSIZE = 2000
+
 documents = []
+lda_model = None
+dct = None
+corpus = None
 
-path = dirname(realpath(__file__)) + "/data/14d9"
-files = [f for f in listdir(path) if isfile(join(path, f))]
 
-for file in files:
-    f = open(path + '/' + file, "r")
-    for row in f:
-        document = [word for word in row.split(" ")]
-        documents.append(document)
+def build_lda_model():
+    path = dirname(realpath(__file__)) + "/data/14d9"
+    files = [f for f in listdir(path) if isfile(join(path, f))]
 
-bigram = Phrases(documents, min_count=20)
-#trigram = Phrases(bigram[documents], threshold=10)
-for idx in range(len(documents)):
-    for token in bigram[documents[idx]]:
-        if '_' in token:
-            documents[idx].append(token)
+    for file in files[:100]:
+        f = open(path + '/' + file, "r", encoding="latin-1")
+        for row in f:
+            document = [word for word in row.split(" ") if len(word) > 2]
+            documents.append(document)
 
-# Dictionary and Corpus
-dct = corpora.Dictionary(documents)
-dct.filter_extremes(no_below=20, no_above=0.5)
-corpus = [dct.doc2bow(line) for line in documents]
+    bigram = Phrases(documents, threshold=10)
+    #trigram = Phrases(bigram[documents], threshold=10)
+    for idx in range(len(documents)):
+        for token in bigram[documents[idx]]:
+            if '_' in token:
+                documents[idx].append(token)
 
-# Train LDA model
-lda_model = LdaModel(corpus=corpus,
-                         id2word=dct,
-                         random_state=100,
-                         num_topics=NUM_TOPICS,
-                         passes=NUM_PASSES,
-                         chunksize=CHUNKSIZE,
-                         iterations=ITERATIONS,
-                         alpha='auto',
-                         eta='auto',
-                         eval_every=None)
+    # Dictionary
+    dct = corpora.Dictionary(documents)
+    dct.filter_extremes(no_below=20, no_above=0.5)
 
-print("LDA Model")
-for idx in range(NUM_TOPICS):
-    print("Topic #%s-" % idx, lda_model.print_topic(idx, 10))
+    # Corpus
+    corpus = [dct.doc2bow(line) for line in documents]
 
+    mallet_path = dirname(realpath(__file__)) + "/mallet-2.0.8/bin/mallet"
+    environ['MALLET_HOME'] = dirname(realpath(__file__)) + '/mallet-2.0.8/'
+    print(mallet_path)
+    lda_mallet = LdaMallet(mallet_path, corpus=corpus, num_topics=NUM_TOPICS, id2word=dct, iterations=ITERATIONS)
+
+    # Show Topics
+    print("LDA Model MALLET")
+    for idx in range(NUM_TOPICS):
+        print("Topic #%s-" % idx, lda_mallet.print_topic(idx, 10))
+
+    lda_model.save('lda_model.model')
+
+
+if __name__ == '__main__':
+    build_lda_model()
--- fetch_documents.py
+++ fetch_documents.py
@@ -4,6 +4,7 @@ import os
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 import string
+import re
 
 stop = set(stopwords.words('english'))
 exclude = set(string.punctuation)
@@ -11,16 +12,16 @@ lemma = WordNetLemmatizer()
 
 
 def cleanse(doc):
-    stop_num_free = " ".join([i for i in doc.lower().split() if i not in stop])
-    punc_free = "".join(ch for ch in stop_num_free if ch not in exclude and len(ch) < 30)
+    non_ascii_stop_free = " ".join([re.sub(r'[^\x00-\x7f]', r'', i) for i in doc.lower().split() if i not in stop])
+    punc_free = "".join(ch for ch in non_ascii_stop_free if ch not in exclude and len(ch) > 2)
     normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
     return normalized
 
 
 if __name__ == '__main__':
     # Azure blob credentials for LDAG
-    account_name='stanfordproject'
-    account_key='Brs84kSEd9yycVvTvbjuGxYlqBsROZHFi2CHV48tRDGO5 + jFwG83j0hraXPDogAeFZNUlBLKMCyhBeX / iWo9bA =='
+    account_name = 'stanfordproject'
+    account_key = 'Brs84kSEd9yycVvTvbjuGxYlqBsROZHFi2CHV48tRDGO5 + jFwG83j0hraXPDogAeFZNUlBLKMCyhBeX / iWo9bA =='
 
     # Create the BlockBlockService to call the Blob service for the storage account
     block_blob_service = BlockBlobService(account_name, account_key)
@@ -28,8 +29,8 @@ if __name__ == '__main__':
     # Get all containers
     containers = block_blob_service.list_containers()
     containers_name = [c.name for c in containers]
-    #print("Available containers: {}".format(containers_name))
-    #['14d9', 'earnings-call', 'ldag', 'meta', 'news', 'proxystatement']
+    # print("Available containers: {}".format(containers_name))
+    # ['14d9', 'earnings-call', 'ldag', 'meta', 'news', 'proxystatement']
 
     # check 14d9 container
     generator = block_blob_service.list_blobs(containers_name[0])
@@ -44,7 +45,7 @@ if __name__ == '__main__':
             soup = BeautifulSoup(raw_text, 'html.parser')
             # remove html tags
             text_only = soup.get_text()
-            # remove stopwords and punctuations
+            # remove non-ascii characters, stopwords and punctuations
             processed_text = cleanse(text_only)
             file_name = os.path.dirname(os.path.realpath(__file__)) + '/data/14d9/' + ''.join(blob.name.split('/')[1:])
             with open(file_name, 'a') as f:
